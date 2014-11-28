aea564232ca9e5a34f43ac17a56d70913329844b
toyofuku tsuyoshi <toyofuku.tsuyoshi@gmai.com>
2014-11-28 22:15:19 +0900

    test-expression

--- 2.3.1/test/test-expression.js
+++ 2.3.1/test/test-expression.js
@@ -1,18 +1,6 @@
-
-var assert = require('assert');
-
 var Number = require('../lib/number');
 var Add = require('../lib/add');
 var Multiply = require('../lib/multiply');
-var Machine = require('../lib/machine-0');
-var LessThan = require('../lib/less-than');
-var Variable = require('../lib/variable');
-var Assign = require('../lib/assign');
-var Boolean = require('../lib/boolean');
-var DoNothing = require('../lib/do-nothing');
-var If = require('../lib/if');
-var Sequence = require('../lib/sequence');
-var While = require('../lib/while');
 
 var expression = new Add(
   new Multiply(
